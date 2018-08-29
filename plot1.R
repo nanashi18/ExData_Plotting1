9f5a3e354b6ce646aabab18656c322a7f67c26ef
Teng Zhang <tengz@seas.upenn.edu>
2018-08-29 13:20:31 +0800

    update

--- plot1.R
+++ plot1.R
@@ -1,8 +1,6 @@
 powerdata <- read.csv("household_power_consumption.txt",header = TRUE, sep=";")
 subpowerdata <- powerdata[powerdata$Date=="2/2/2007" | powerdata$Date=="1/2/2007",]
-pwr <- as.numeric(subpowerdata$Global_active_power)
-pwr1 <- lapply(pwr, (function(x) (as.numeric(x)/1000)))
-pwr11 <- unlist(pwr1, use.names=FALSE)
+pwr <- (as.numeric(as.character(subpowerdata$Global_active_power)))
 png("plot1.png", width=480, height=480)
-hist(pwr11,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(pwr,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,9 +1,7 @@
 powerdata <- read.csv("household_power_consumption.txt",header = TRUE, sep=";")
 subpowerdata <- powerdata[powerdata$Date=="2/2/2007" | powerdata$Date=="1/2/2007",]
-pwr <- as.numeric(subpowerdata$Global_active_power)
-pwr1 <- lapply(pwr, (function(x) (as.numeric(x)/1000)))
-pwr11 <- unlist(pwr1, use.names=FALSE)
+pwr <- (as.numeric(as.character(subpowerdata$Global_active_power)))
 dt <- strptime(paste(subpowerdata$Date, subpowerdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 png("plot2.png", width=480, height=480)
-plot(dt, pwr11, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(dt, pwr, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
